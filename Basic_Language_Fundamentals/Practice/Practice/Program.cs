4a28bfc960dadc1e98509b269aa27c0b50ebc69b
harishsune <harishsune@hotmail.com>
2017-02-08 01:21:36 +0530

    Added basic c# programs

---
+++ Basic_Language_Fundamentals/Practice/Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "Five Star";
+            String s2 = "FIVE STAR";
+            int c;
+            c = s1.CompareTo(s2);
+            Console.WriteLine(c);
+            Console.Read();
+        }
+    }
+}
